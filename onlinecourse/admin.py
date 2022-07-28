8d95ab77ab675df23e6eb9d9257767253c04bdb3
Khanh-Tuan <107778275+Khanh-Tuan@users.noreply.github.com>
2022-07-29 00:19:22 +1200

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -21,6 +21,9 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    fields = ('question_text', 'grade', 'lesson_id')
 
 # <HINT> Register Question and Choice models here
 
@@ -28,3 +31,5 @@ admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
